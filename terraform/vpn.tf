11b4c2f030a2583450a84806801b1903e64d1d14
Melo <julio.alexandre.melo@accenture.com>
2018-08-23 18:11:59 -0300

    Correction

--- terraform/vpn.tf
+++ terraform/vpn.tf
@@ -3,13 +3,14 @@ resource "aws_key_pair" "auth" {
 }
 
 resource "aws_instance" "vpn" {
-  instance_type = "t2.micro"
-  ami = "ami-97785bed"
+  instance_type               = "t2.micro"
+  ami                         = "ami-97785bed"
   associate_public_ip_address = true
+
   # Tags to indentify the instace
-  tags{
-    Name= "CMT VPN"
-    tag = "CMT VPN"
+  tags {
+    Name = "VPN"
+    tag  = "VPN"
   }
 
   # The name of our SSH keypair
@@ -21,16 +22,16 @@ resource "aws_instance" "vpn" {
   subnet_id = "${aws_subnet.cmt-public-sub.id}"
 
   # force Terraform to wait until a connection can be made, so that Ansible doesn't fail when trying to provision
- # provisioner "remote-exec" {
-    # The connection will use the local SSH agent for authentication
-    # inline = ["echo VPN successfully connected"]
+  # provisioner "remote-exec" {
+  # The connection will use the local SSH agent for authentication
+  # inline = ["echo VPN successfully connected"]
 
-    # The connection block tells our provisioner how to communicate with the resource (instance)
-    #connection {
-    #  user = "${var.ec2-user}"
-    #  private_key = "${file(var.private_key)}"
-    #  host = "${aws_instance.vpn.public_ip}"
-    #  timeout = "5m"
-    # }
- # }
+  # The connection block tells our provisioner how to communicate with the resource (instance)
+  #connection {
+  #  user = "${var.ec2-user}"
+  #  private_key = "${file(var.private_key)}"
+  #  host = "${aws_instance.vpn.public_ip}"
+  #  timeout = "5m"
+  # }
+  # }
 }
